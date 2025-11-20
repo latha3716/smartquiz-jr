e07d1bd5bf1120301510f4792b90901a98f4304b
Vanjarapu Chandan Kumar <gangaputrachandan@gmail.com>
2025-11-20 12:22:04 +0530

    misc

--- smartquizjr_backend/app/routes/ai_routes.py
+++ smartquizjr_backend/app/routes/ai_routes.py
@@ -10,7 +10,7 @@ router = APIRouter(
     tags=["AI Logic"]
 )
 
-@router.get("/create", status_code=201)
+@router.post("/create", status_code=201)
 async def create_AI_questions_with_topic(ai_request: AIRequest, db: Session = Depends(get_db)):
     return create_questions(ai_request.topic, ai_request.age, ai_request.difficulty, ai_request.questions)
     
