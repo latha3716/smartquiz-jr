38f3962bb44cc4822196de458943b7190fe9a3f1
Vanjarapu Chandan Kumar <gangaputrachandan@gmail.com>
2025-11-21 08:51:41 +0530

    validate question before adding

--- smartquizjr_backend/app/routes/ai_routes.py
+++ smartquizjr_backend/app/routes/ai_routes.py
@@ -17,7 +17,8 @@ async def create_AI_questions_with_topic(ai_request: AIRequest, db: Session = De
         ai_request.topic, 
         ai_request.age, 
         ai_request.difficulty, 
-        ai_request.questions
+        ai_request.questions,
+        db=db
     )
 
     
